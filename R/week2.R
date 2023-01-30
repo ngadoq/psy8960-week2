71c1c10edfc20d043b79525be5bc47c9832604b8
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-01-30 12:29:52 -0600

    # class solution

--- R/week2.R
+++ R/week2.R
@@ -1,11 +1,12 @@
 # Import and Labeling
-rt_df <- read.csv(file = "./data/week2.csv", header = TRUE)
+rt_df <- read.csv(file = "data/week2.csv", header = TRUE)
 rt_df$condition <- factor(rt_df$condition, levels = c("A", "B"), labels = c("Control", "Experiment"))
 rt_df$gender <- factor(rt_df$gender, levels = c("M", "F", "N"), labels = c("Male", "Female",  "Nonbinary"))
 
 # Analysis
 mean(rt_df$rt)
-rt_f_df <- subset(rt_df, gender == "Female")
+rt_f_df <- rt_df[rt_df$gender == "Female",] #don't use subset 
 hist(rt_f_df$rt)
 datasets <- list(rt_df, rt_f_df)
-datasets[[1]]$rt
+datasets[[1]]$rt  #datasets[1] return a list 
+
